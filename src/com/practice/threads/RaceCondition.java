9781dacdf5df2ce8f611df44830e7e5ab82a3ba5
Sharvari <sharvari.kapadia@gmail.com>
2015-07-06 21:58:14 -0700

    Implementation of SPSC (single producer single consumer)

--- src/com/practice/threads/RaceCondition.java
+++ src/com/practice/threads/RaceCondition.java
@@ -1,9 +1,28 @@
 package com.practice.threads;
 
-public class RaceCondition {
+public class RaceCondition implements Runnable {
+	
+	public int count;
+	
+	RaceCondition() {
+		count = 0;
+	}
+	
+	@Override
+	public void run() {
+		synchronized(this) {
+			for(int i = 0; i < 100000; i++) {
+				count++;
+			}
+		}
+	}
+	
+	public void printCount() {
+		System.out.println(count);
+	}
 	
 	public static void main(String[] args) throws InterruptedException {
-		Runnable r = new RunnableImpl();
+		Runnable r = new RaceCondition();
 		Thread[] t = new Thread[10];
 		
 		for (int i = 0; i < 10; i++) {
@@ -15,6 +34,7 @@ public class RaceCondition {
 			//t[i].join();
 		}
 		
-		((RunnableImpl) r).printCount();	
+		((RaceCondition) r).printCount();	
 	}
+	
 }
