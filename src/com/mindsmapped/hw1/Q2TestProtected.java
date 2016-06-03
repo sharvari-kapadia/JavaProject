d702964e8047a497f7dc08dfca579c6f7110c253
Sharvari <sharvari.kapadia@gmail.com>
2016-06-03 16:58:36 -0700

    Removing mindsmapped package

--- src/com/mindsmapped/hw1/Q2TestProtected.java
+++
@@ -1,9 +0,0 @@
-package com.mindsmapped.hw1;
-
-public class Q2TestProtected {
-    protected String rollno = "hello";
-
-    protected void printRollNo() {
-        System.out.println(rollno);
-    }
-}
