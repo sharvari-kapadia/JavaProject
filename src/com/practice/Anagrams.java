98e51958a03cfd3e9602f91161869e499f758544
Sharvari <sharvari.kapadia@gmail.com>
2016-07-20 11:59:04 -0700

    Adding solution to dumpList and getAnagramsList

---
+++ src/com/practice/Anagrams.java
@@ -0,0 +1,50 @@
+/*
+* input = {pots, car, stop, god, dog, bare, door, bear, balm, lamb, looped, man,poodle}
+  output = {{pots, stop}, {dog, god}, {bare, bear}, {balm, lamb}, {looped, poodle}
+* */
+
+package com.practice;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * Created by SKapadia on 7/20/2016.
+ */
+public class Anagrams {
+
+    public ArrayList<ArrayList<String>> getAnagramsList(ArrayList<String> list) {
+
+        HashMap<String, ArrayList<String>> hashMap = new HashMap<>();
+        ArrayList<String> tempList;
+        String tempString;
+        for(String string : list) {
+            char[] stringAsCharArray = string.toCharArray();
+            Arrays.sort(stringAsCharArray);
+            tempString = new String(stringAsCharArray);
+            if(hashMap.containsKey(tempString)) {
+                tempList = hashMap.get(tempString);
+                tempList.add(string);
+                hashMap.put(tempString, tempList);
+            } else {
+                hashMap.put(tempString, new ArrayList<>(Arrays.asList(string)));
+            }
+        }
+
+        ArrayList<ArrayList<String>> resultList = new ArrayList<>();
+        for(ArrayList<String> arrayList: hashMap.values()) {
+            if(arrayList.size() > 1) {
+                resultList.add(arrayList);
+            }
+        }
+        return resultList;
+    }
+
+    public static void main(String[] args) {
+
+        ArrayList<ArrayList<String>> resultList = new Anagrams().getAnagramsList(new ArrayList<>(Arrays.asList(
+            "pots", "car", "stop", "god", "dog", "bare", "door", "bear", "balm", "lamb", "looped", "man", "poodle")));
+        System.out.println(resultList.toString());
+    }
+}
---
+++ src/com/practice/PrintNDimensionalList.java
@@ -0,0 +1,57 @@
+package com.practice;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created by SKapadia on 7/19/2016.
+ */
+public class PrintNDimensionalList {
+
+    public void dumpList(ArrayList<Object> list, String string) {
+
+        _dumpList(list, string, new ArrayList<>());
+    }
+
+    private void _dumpList(ArrayList<Object> list, String string, ArrayList<Integer> prefix) {
+
+        for(int i = 0; i < list.size(); i++) {
+            prefix.add(i);
+            Object object = list.get(i);
+            if(object instanceof String) {
+                print(string, prefix, (String)object);
+            } else if(object instanceof ArrayList) {
+                _dumpList((ArrayList) object, string, prefix);
+            }
+            prefix.remove(prefix.size() - 1);
+        }
+    }
+
+    private void print(String string, ArrayList<Integer> prefix, String item) {
+
+        StringBuffer toPrint = new StringBuffer(string);
+        for(int i : prefix) {
+            toPrint.append("." + i);
+        }
+        toPrint.append(": " + item);
+        System.out.println(toPrint);
+    }
+
+    public static void main(String[] args) {
+
+
+        ArrayList<Object> object = new ArrayList<>();
+        object.add("a string");
+        object.add(new ArrayList<>(Arrays.asList("a", "b", "c")));
+        object.add("spam");
+        object.add(new ArrayList<>(Arrays.asList("eggs")));
+
+        ArrayList<Object> object2 = new ArrayList<>();
+        object2.add(new ArrayList<>(Arrays.asList("a1", "b1", "c1")));
+        object2.add(new ArrayList<>(Arrays.asList("a2", "b2", "c2")));
+
+        object.add(object2);
+
+        new PrintNDimensionalList().dumpList(object, "Foo");
+    }
+}
