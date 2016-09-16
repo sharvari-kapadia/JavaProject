520506dce8061ec62685f9afb58f855fc74d766c
Sharvari <sharvari.kapadia@gmail.com>
2016-09-15 17:53:01 -0700

    Solution to leetcode problems - RemoveDuplicatesFromSortedList.java

---
+++ src/com/leetcode/easy/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,38 @@
+package com.leetcode.easy;
+
+/**
+ * Created by SKapadia on 9/15/2016.
+ */
+public class RemoveDuplicatesFromSortedList {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+
+        if(head == null) {
+            return null;
+        }
+
+        ListNode p1 = head;
+        ListNode p2 = head.next;
+
+        while(p1 != null && p2 != null) {
+
+            if(p2.val == p1.val) {
+
+                p1.next = p2.next;
+                p2.next = null;
+                p2 = p1.next;
+            } else {
+                p1 = p1.next;
+                p2 = p2.next;
+            }
+        }
+
+        return head;
+    }
+}
